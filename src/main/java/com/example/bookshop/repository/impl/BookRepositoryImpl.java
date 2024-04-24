5aa477a99b6df22739fe5a3d0f5f9a531bb94923
Pavlo_Betsa <151930464+Nikname2303@users.noreply.github.com>
2024-04-24 15:18:20 +0300

    Book dto (#2)
    * Added DTO, controller and findById
    
    * Added DTO, controller and findById
    
    * improves after review - endpoint api/books and lombok without version
    
    * improves after review - endpoint api/books and lombok without version
    
    * improves after review - lombok version in properties

--- src/main/java/com/example/bookshop/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/bookshop/repository/impl/BookRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.example.bookshop.repository.impl;
 
 import com.example.bookshop.exception.DataProcessingException;
+import com.example.bookshop.exception.EntityNotFoundException;
 import com.example.bookshop.model.Book;
 import com.example.bookshop.repository.BookRepository;
 import jakarta.persistence.EntityManager;
@@ -8,17 +9,14 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
     private final EntityManagerFactory entityManagerFactory;
-
-    @Autowired
-    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
-    }
 
     @Override
     public Book save(Book book) {
@@ -49,6 +47,16 @@ public class BookRepositoryImpl implements BookRepository {
             return fromBook.getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't get books ", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            Book book = entityManager.find(Book.class, id);
+            return Optional.ofNullable(book);
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Can't find book with id: " + id);
         }
     }
 }
