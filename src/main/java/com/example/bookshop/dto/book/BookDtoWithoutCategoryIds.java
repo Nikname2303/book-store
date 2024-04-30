7cfeafb7f5413c1b1180e8c2ab5c0e04f769e401
Pavlo Betsa <bigworld2303@gmail.com>
2024-04-30 16:28:56 +0300

    Added CustomGlobalExceptionHandler and validate logic for RequestDto

--- src/main/java/com/example/bookshop/dto/book/BookDtoWithoutCategoryIds.java
+++ src/main/java/com/example/bookshop/dto/book/BookDtoWithoutCategoryIds.java
@@ -1,12 +1,19 @@
 package com.example.bookshop.dto.book;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public class BookDtoWithoutCategoryIds {
     private Long id;
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
