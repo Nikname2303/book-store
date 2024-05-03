8ff1e11f92f80941d9de9f7613dc1245f7379b25
Pavlo_Betsa <151930464+Nikname2303@users.noreply.github.com>
2024-05-03 13:25:45 +0300

    Added CustomGlobalExceptionHandler and validate logic for RequestDto (#4)
    * Added CustomGlobalExceptionHandler and validate logic for RequestDto
    
    * improvements in requestdto annotations
    
    * improvements in requestdto annotations
    
    * added handle for entityNotFoundException and Exception
    
    * added timestamp, deleted changes from other HW, solve right order of methods
    
    * added @NotNull for isbn

--- src/main/java/com/example/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookshop/dto/CreateBookRequestDto.java
@@ -1,14 +1,24 @@
 package com.example.bookshop.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
     private Long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @ISBN
+    @NotNull
     private String isbn;
+    @NotNull
+    @PositiveOrZero
     private BigDecimal price;
     private String description;
     private String coverImage;
