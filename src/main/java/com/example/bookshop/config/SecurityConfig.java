527f73b595e9ba54c3b3625beabfd37d42111e5a
Pavlo Betsa <bigworld2303@gmail.com>
2024-05-14 15:48:38 +0300

    Added AuthenticationService, JwtAuthenticationFilter, JwtUtil, AuthenticationController

--- src/main/java/com/example/bookshop/config/SecurityConfig.java
+++ src/main/java/com/example/bookshop/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.bookshop.config;
 
+import com.example.bookshop.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +12,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -39,6 +42,7 @@ public class SecurityConfig {
                                 .authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
     }
