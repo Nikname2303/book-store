9353c19bf33ebbd7af4293a81f0485b03c638772
Pavlo Betsa <bigworld2303@gmail.com>
2024-05-15 11:44:36 +0300

    added role user for new registration

--- src/main/java/com/example/bookshop/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/bookshop/service/impl/UserServiceImpl.java
@@ -4,9 +4,13 @@ import com.example.bookshop.dto.user.UserRegistrationRequestDto;
 import com.example.bookshop.dto.user.UserResponseDto;
 import com.example.bookshop.exception.RegistrationException;
 import com.example.bookshop.mapper.UserMapper;
+import com.example.bookshop.model.Role;
 import com.example.bookshop.model.User;
+import com.example.bookshop.repository.RoleRepository;
 import com.example.bookshop.repository.UserRepository;
 import com.example.bookshop.service.UserService;
+import java.util.Collections;
+import java.util.HashSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
@@ -25,7 +30,15 @@ public class UserServiceImpl implements UserService {
         }
         String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
         requestDto.setPassword(encodedPassword);
-        User savedUser = userRepository.save(userMapper.toModelFromRegisterDto(requestDto));
+        Role.RoleName roleName = Role.RoleName.USER;
+        Role role = roleRepository.findByName(roleName)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Default role not found. Check database initialization."));
+
+        User user = userMapper.toModelFromRegisterDto(requestDto);
+        user.setRoles(new HashSet<>(Collections.singletonList(role)));
+
+        User savedUser = userRepository.save(user);
         return userMapper.toResponseDto(savedUser);
     }
 }
