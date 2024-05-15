e60c3fc29bff8eb87951550d170138535578c4a8
Pavlo_Betsa <151930464+Nikname2303@users.noreply.github.com>
2024-05-15 14:08:21 +0300

    Added AuthenticationService, JwtAuthenticationFilter, JwtUtil, Authen… (#8)
    * Added CustomGlobalExceptionHandler and validate logic for RequestDto
    
    * improvements in requestdto annotations
    
    * improvements in requestdto annotations
    
    * added handle for entityNotFoundException and Exception
    
    * solved conflicts
    
    * added descriptions
    
    * Added User entity, DTO`s, controller, method register, RegistrationException
    
    * Added User entity, DTO`s, controller, method register, RegistrationException
    
    * Added FieldMatch annotation
    
    * Changes after review
    
    * added handle for entityNotFoundException and Exception
    
    * Changes after review
    
    * Changes after review, annotation not for specific fields, email-valid
    
    * Added starter security, UserDetails, Roles, SecurityConfig
    
    * Added starter security, UserDetails, Roles, SecurityConfig
    
    * Added hash password, @EnableMethodSecurity and divided changesets
    
    * Added AuthenticationService, JwtAuthenticationFilter, JwtUtil, AuthenticationController
    
    * added jwt.expiration and jwt.secret for tests
    
    * added constants and @Valid
    
    * added role user for new registration

--- src/main/java/com/example/bookshop/controller/AuthenticationController.java
+++ src/main/java/com/example/bookshop/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.bookshop.controller;
 
+import com.example.bookshop.dto.user.UserLoginRequestDto;
+import com.example.bookshop.dto.user.UserLoginResponseDto;
 import com.example.bookshop.dto.user.UserRegistrationRequestDto;
 import com.example.bookshop.dto.user.UserResponseDto;
 import com.example.bookshop.exception.RegistrationException;
+import com.example.bookshop.security.AuthenticationService;
 import com.example.bookshop.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
+        return authenticationService.authenticate(request);
     }
 }
