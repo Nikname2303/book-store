089561fcb99c9b37b357eb28d5f6d630ad825824
Pavlo_Betsa <151930464+Nikname2303@users.noreply.github.com>
2024-05-15 14:03:40 +0300

    Book security login (#7)
    * Added CustomGlobalExceptionHandler and validate logic for RequestDto
    
    * improvements in requestdto annotations
    
    * improvements in requestdto annotations
    
    * added handle for entityNotFoundException and Exception
    
    * solved conflicts
    
    * added descriptions
    
    * Added User entity, DTO`s, controller, method register, RegistrationException
    
    * Added User entity, DTO`s, controller, method register, RegistrationException
    
    * Added FieldMatch annotation
    
    * Changes after review
    
    * added handle for entityNotFoundException and Exception
    
    * Changes after review
    
    * Changes after review, annotation not for specific fields, email-valid
    
    * Added starter security, UserDetails, Roles, SecurityConfig
    
    * Added starter security, UserDetails, Roles, SecurityConfig
    
    * Added hash password, @EnableMethodSecurity and divided changesets
    
    * added role user for new registration

---
+++ src/main/java/com/example/bookshop/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.bookshop.repository;
+
+import com.example.bookshop.model.Role;
+import com.example.bookshop.model.Role.RoleName;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName roleName);
+}
--- src/main/java/com/example/bookshop/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/bookshop/service/impl/UserServiceImpl.java
@@ -4,10 +4,15 @@ import com.example.bookshop.dto.user.UserRegistrationRequestDto;
 import com.example.bookshop.dto.user.UserResponseDto;
 import com.example.bookshop.exception.RegistrationException;
 import com.example.bookshop.mapper.UserMapper;
+import com.example.bookshop.model.Role;
 import com.example.bookshop.model.User;
+import com.example.bookshop.repository.RoleRepository;
 import com.example.bookshop.repository.UserRepository;
 import com.example.bookshop.service.UserService;
+import java.util.Collections;
+import java.util.HashSet;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,13 +20,25 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
         if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
             throw new RegistrationException("User is already exist");
         }
-        User savedUser = userRepository.save(userMapper.toModelFromRegisterDto(requestDto));
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+        requestDto.setPassword(encodedPassword);
+        Role.RoleName roleName = Role.RoleName.USER;
+        Role role = roleRepository.findByName(roleName)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Default role not found. Check database initialization."));
+
+        User user = userMapper.toModelFromRegisterDto(requestDto);
+        user.setRoles(new HashSet<>(Collections.singletonList(role)));
+
+        User savedUser = userRepository.save(user);
         return userMapper.toResponseDto(savedUser);
     }
 }
