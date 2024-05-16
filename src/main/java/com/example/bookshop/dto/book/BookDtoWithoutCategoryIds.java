cf8a5f26cb28704bbb19e642b13daff5b0baf630
Pavlo Betsa <bigworld2303@gmail.com>
2024-05-16 17:03:00 +0300

    Fixed style issues

--- src/main/java/com/example/bookshop/dto/book/BookDtoWithoutCategoryIds.java
+++ src/main/java/com/example/bookshop/dto/book/BookDtoWithoutCategoryIds.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
-import lombok.Data;
 
 public class BookDtoWithoutCategoryIds {
     private Long id;
