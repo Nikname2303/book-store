3399afa1794f2f8cff870db85ab8ee872624479e
Pavlo Betsa <bigworld2303@gmail.com>
2024-09-16 17:23:13 +0300

    Fixed order saving, update methods for order, added Query in shoppingCart repository and minor updates in exception texts

--- src/main/java/com/example/bookshop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/example/bookshop/service/impl/ShoppingCartServiceImpl.java
@@ -32,7 +32,8 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCartResponseDto getCart(Long id) {
         ShoppingCart cart = shoppingCartRepository.getCartByUserId(id).orElseThrow(
-                () -> new EntityNotFoundException("Can't find shopping cart for user with id: " + id)
+                () -> new EntityNotFoundException("Can't find shopping cart for user with id: "
+                        + id)
         );
         return shoppingCartMapper.toDto(cart);
     }
@@ -40,7 +41,8 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCartResponseDto addCartItem(CartItemRequestDto requestDto, Long id) {
         ShoppingCart cart = shoppingCartRepository.getCartByUserId(id).orElseThrow(
-                () -> new EntityNotFoundException("Can't find shopping cart for user with id: " + id)
+                () -> new EntityNotFoundException("Can't find shopping cart for user with id: "
+                        + id)
         );
         Book book = bookRepository.findById(requestDto.getBookId()).orElseThrow(
                 () -> new RuntimeException("Book with id "
@@ -62,7 +64,8 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             CartItemUpdateDto updateDto
     ) {
         ShoppingCart shoppingCart = shoppingCartRepository.getCartByUserId(id).orElseThrow(
-                () -> new EntityNotFoundException("Can't find shopping cart for user with id: " + id)
+                () -> new EntityNotFoundException("Can't find shopping cart for user with id: "
+                        + id)
         );
         CartItem cartItem = shoppingCart.getCartItems().stream()
                 .filter(e -> e.getId().equals(cartItemId))
